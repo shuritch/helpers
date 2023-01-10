5f2dcd2fbfda5a17c40910836c13c85d25a64fcb
sashapop10 <sashapop10@yandex.ru>
2023-01-11 00:48:50 +0300

    remove

--- rules/ecmascript-13.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-  rules: {
-    'no-unused-private-class-members': ['error'],
-    'no-invalid-this': ['error'],
-    'class-methods-use-this': ['warn'],
-  },
-};
